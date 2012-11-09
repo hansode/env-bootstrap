94797672f198992199cbcaaf08177c4c4cee6435
Masahito Yoshida <masahito@axsh.net>
2012-11-09 15:53:38 +0900

    use strict bash syntax.

--- add-work-user.sh
+++ add-work-user.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 #
+# requiers:
+#  bash
+#  dirname, pwd
+#  detect-linux-distribution.sh
+#  getent, groupadd, useradd, chpasswd
+#  cat, egrep
 #
-#
-export LANG=C
-export LC_ALL=C
-
 set -e
-abs_path=$(cd $(dirname $0) && pwd)
-eval `${abs_path}/detect-linux-distribution.sh`
 
-devel_user=$(echo ${DISTRIB_ID} | tr A-Z a-z)
-devel_group=${devel_user}
-devel_home=/home/${devel_user}
+## variables
+
+readonly abs_dirname=$(cd $(dirname $0) && pwd)
+
+eval `${abs_dirname}/detect-linux-distribution.sh`
+
+declare devel_user=$(echo ${DISTRIB_ID} | tr A-Z a-z)
+declare devel_group=${devel_user}
+declare devel_home=/home/${devel_user}
+
+## main
 
 cat <<EOS
 devel_user=${devel_user}
@@ -19,14 +27,13 @@ devel_group=${devel_user}
 devel_home=/home/${devel_user}
 EOS
 
-#
 getent group  ${devel_group} >/dev/null || groupadd ${devel_group}
 getent passwd ${devel_user}  >/dev/null || useradd -g ${devel_group} -d ${devel_home} -s /bin/bash -m ${devel_user}
 echo ${devel_user}:${devel_user} | chpasswd
 
-devel_group=$(getent group ${devel_group} | awk -F: '{print $1}')
-devel_home=$(getent passwd ${devel_user} | awk -F: '{print $6}')
-work_dir=${devel_home}/work
+declare devel_group=$(getent group ${devel_group} | awk -F: '{print $1}')
+declare devel_home=$(getent passwd ${devel_user}  | awk -F: '{print $6}')
+declare work_dir=${devel_home}/work
 
 cat <<EOS
 devel_user  = ${devel_user}
@@ -35,10 +42,8 @@ devel_home  = ${devel_home}
 work_dir    = ${work_dir}
 EOS
 
-[ -f /etc/sudoers ] && {
+[[ -f /etc/sudoers ]] && {
   egrep ^${devel_user} /etc/sudoers -q || {
    echo "${devel_user} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
   }
 }
-
-exit 0
